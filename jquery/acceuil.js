4c81a3f31effaa9002b8995c828852596c5914c8
Enymoduesp <jan.paquet.inutile@gmail.com>
2025-11-23 14:41:02 +0100

    Delete jquery directory

--- jquery/acceuil.js
+++
@@ -1,3 +0,0 @@
- $(document).ready(function(){$("#duellogo").click(function(){$("main").load("include/duel.html", function(){
-$.getScript("jquery/duel.js")})}) 
-});  /*pour charger la page duel dynamiquement*/
