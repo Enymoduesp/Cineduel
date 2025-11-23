abc5fde73fcb0994d81263112ce7d2ea70f21a30
Enymoduesp <jan.paquet.inutile@gmail.com>
2025-11-23 14:35:10 +0100

    Add files via upload

---
+++ jquery/acceuil.js
@@ -0,0 +1,3 @@
+ $(document).ready(function(){$("#duellogo").click(function(){$("main").load("include/duel.html", function(){
+$.getScript("jquery/duel.js")})}) 
+});  /*pour charger la page duel dynamiquement*/
